7492da073fca41a259daeaa396b642d0f57248e9
Addiel <addieljuarez@gmail.com>
2016-06-17 01:42:36 -0500

    finish

--- popup_script.js
+++ popup_script.js
@@ -1,11 +1,25 @@
 
 
-var timeId = "time";
-var dateId = "date";
 var days = ["sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 var consoleGreating = "Hola mundo desde JS";
+// alert('test');
 
-function setTimeAndDate(){
+function setTimeAndDate(timeElement, dateElement){
 	var date = new Date();
+	var minutes = (date.getMinutes() < 10 ? '0' : '') + date.getMinutes();
+	var time = date.getHours() + ":" + minutes;
+
+	var date1 = days[date.getDay()] + ", " + months[date.getMonth()] + " " + date.getDate() + " " + date.getFullYear();
+	timeElement.innerHTML = time;
+	dateElement.innerHTML = date1;
 }
+
+
+console.log(consoleGreating);
+
+document.addEventListener('DOMContentLoaded', function(dcle){
+	 var timeElement = document.getElementById('time');
+	 var dateElement = document.getElementById('date');
+	 setTimeAndDate(timeElement, dateElement)
+})
