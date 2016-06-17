6a558f4fcdfc246670b87be63823a17c1addc7a6
Addiel <addieljuarez@gmail.com>
2016-06-17 00:43:03 -0500

    init

---
+++ popup_script.js
@@ -0,0 +1,11 @@
+
+
+var timeId = "time";
+var dateId = "date";
+var days = ["sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var consoleGreating = "Hola mundo desde JS";
+
+function setTimeAndDate(){
+	var date = new Date();
+}
